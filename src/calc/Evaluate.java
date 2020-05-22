7b4311eb3953f36bfd10164f9638b48c9b38aabc
Veera Sivarajan <sveera.2001@gmail.com>
2020-05-22 01:10:08 -0400

    Infix to Postfix Done

--- src/calc/Evaluate.java
+++ src/calc/Evaluate.java
@@ -1,33 +1,60 @@
 package calc;
 
 public class Evaluate {
-  private Stack<T> operator;
+  private Stack<Character> operator;
   private String output;
-
-  public static void analyze(char input) { 
-    if(Character.isDigit(input)) {
-      output += input;
-    }
-    else {
-      if(precedence(input) > precedence(operator.peek())) {
-        operator.push(input);
-      }
-      else {
-        while(precedence(input) <= precedence(operator.peek())) {
-          stack.pop();
-        }
-        operator.push(input);
-      }
-    }
-    }
+  
+  public Evaluate() {
+    operator = new Stack<Character>();
+    output = new String("");
   }
 
-
-
-  public static void main(String[] args) {
-    String input = args[0];
-    for(int i = 0; i < input.length(); ++i) {
-      Evaluate.analyze(input.charAt(i));
+  private int precedence(char ch) {
+    int result = 0;
+    switch(ch) {
+      case '+':
+      case '-':
+        result = 1;
+        break;
+      case '*':
+      case '/':
+        result = 2;
+        break;
     }
+    return result;
+  }
+
+  public String convert(String input) throws Exception{ 
+    for(int i = 0; i < input.length(); ++i) {
+      char ch = input.charAt(i);
+      if(Character.isDigit(ch)) {
+        output += ch;
+      }
+
+      else if(Character.compare(ch, '(') == 0) {
+        operator.push(ch);
+      }
+      else if(Character.compare(ch, ')') == 0) {
+        while(operator.getSize() > 0 && Character.compare(operator.peek(), '(') != 0) {
+          output += operator.pop();
+        }
+        operator.pop(); //popping '('
+      }
+      else {
+        while(operator.getSize() > 0 && precedence(ch) <= precedence(operator.peek())) {
+          output += operator.pop();
+        }
+        operator.push(ch);
+      }
+    }
+    while(operator.getSize() > 0) {
+      output += operator.pop();
+    }
+    return output;
+  }
+
+  public static void main(String[] args) throws Exception {
+    Evaluate eval = new Evaluate();
+    System.out.println("Output: " + eval.convert(args[0]));
   }
 }
